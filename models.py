d8b2c1c78c8ed04936fc14eff0819dea4c9217bf
Niranjan Shah <niranjanshah474@gmail.com>
2023-06-08 16:44:39 +1000

    Version 1 of the model trained

--- models.py
+++ models.py
@@ -8,11 +8,22 @@ class PlantDiseaseDetector(nn.Module):
     """
     def __init__(self, num_classes):
         super().__init__()
-        self.model = models.resnet50(pretrained=True)
+        weights = models.ResNet50_Weights
+        self.model = models.resnet50(weights=weights)
         # for freezing the resnet model
         for param in self.model.parameters():
             param.requires_grad = False
-        self.model.fc = nn.Linear(2048, num_classes)
+        self.fc1 = nn.Linear(2048, 1024)
+        self.relu = nn.ReLU()
+        self.dout = nn.Dropout(0.5)
+        self.bn1 = nn.BatchNorm1d(1024)
+        self.fc2 = nn.Linear(1024, num_classes)
+
 
     def forward(self,x):
-        return self.model(x)
+        x = self.model(x)
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.dout(x)
+        x = self.bn1(x)
+        return self.fc2(x)
