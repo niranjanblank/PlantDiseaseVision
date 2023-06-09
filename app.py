a089ecda593c775f8bac61581af7ef528aa9121c
Niranjan Shah <niranjanshah474@gmail.com>
2023-06-09 20:34:50 +1000

    Transform fixed from 2224 to 224

--- app.py
+++ app.py
@@ -26,11 +26,12 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 plant_model = torch.load('trained_models/model_full.pth',  map_location=device)
 plant_model.to(device)
 # Define the transformation
+# Creating image transformers
 transform = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(224),
+    transforms.Resize(size=256),
+    transforms.CenterCrop((224,224)),
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
 
 # Load the class name
