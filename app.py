4967c497cc1b5cad0fdcb446897f86673c51ee75
Niranjan Shah <niranjanshah474@gmail.com>
2023-06-10 18:31:24 +1000

    Changes made to streamlit app like added comments, added theming, added markdowns

--- app.py
+++ app.py
@@ -12,8 +12,15 @@ with open(file_path, "r") as file:
     json_data = json.load(file)
 
 
+
+
 # function to search by id
 def search_by_id(id_value, data):
+    """
+    Function to give information regarding plant disease by id
+    id_value: id given by the model
+    data: josn data containting information
+    """
     for item in data:
         if item["id"] == id_value:
             return item
@@ -22,13 +29,14 @@ def search_by_id(id_value, data):
 
 # setting device
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
 # instantiate the model
+# 38 is used as there are 38 classes and model was trained on 38 classes
 plant_model = PlantDiseaseDetector(38)
-# plant_model = torch.load('trained_models/model_full.pth',  map_location=device)
 plant_model.load_state_dict(torch.load('trained_models/plant_model_v3.pth'))
 plant_model.to(device)
-# Define the transformation
-# Creating image transformers
+
+# Creating image transformers similar to one used in training
 transform = transforms.Compose([
     transforms.Resize(size=256),
     transforms.CenterCrop((224,224)),
@@ -36,13 +44,16 @@ transform = transforms.Compose([
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
 
-# Load the class name
-with open('classes.pkl', 'rb') as f:
-    loaded_classes = pickle.load(f)
 
-st.title("Plant Disease Detector")
-upload= st.file_uploader("Choose an image..", type=["jpg", "png", "jpeg", "webp"])
+st.set_page_config(page_title="Plant Disease Vision",page_icon="🌱")
 
+# Title of the app
+st.title("PlantDiseaseVision")
+
+# uploading the image we want to predict on
+upload = st.file_uploader("Choose an image of plant disease..", type=["jpg", "png", "jpeg", "webp"])
+
+predicted_class = None
 if upload is not None:
     img = Image.open(upload)
     st.image(img, caption="Uploaded Image", use_column_width=True)
@@ -58,4 +69,16 @@ if upload is not None:
     _, predicted = torch.max(out.data, 1)
     ## the infromation related to the predicted class is retrieved
     predicted_class = search_by_id(predicted.item(), json_data)
-    st.write("Predicted Class: ",predicted_class)
+if predicted_class:
+    st.header("Predicted : "+predicted_class["name"])
+    st.divider()
+    st.header("Solutions: ")
+    for item in predicted_class["solutions"]:
+        st.markdown(f"* {item.capitalize()}")
+    # st.write(predicted_class["solutions"])
+
+st.divider()
+st.markdown("""
+Made with :heart: by Niranjan Shah \n
+Repo: https://github.com/niranjanblank/PlantDiseaseVision
+""")
